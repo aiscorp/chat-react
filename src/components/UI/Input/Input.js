66e5680abe2d05914959d7b0cbec86d1de2543c9
aiscorp <aiscorp@yandex.ru>
2020-09-07 23:51:25 +0300

    UI/Input (1-line & 2-line), first var

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,41 @@
+import React from "react"
+import classes from './Input.module.scss'
+
+const Input = props => {
+  const type = props.type || 'text'
+  const cls = [
+    classes.Input,
+    classes[type]
+  ]
+
+  return (
+    type === 'two-line' ?
+      <textarea className={cls.join(' ')}
+                onKeyPress={props.onKeyPress}
+                placeholder={props.placeholder}
+                value={props.value}/>
+      :
+      <input type={type}
+             className={cls.join(' ')}
+             onKeyPress={props.onKeyPress}
+             placeholder={props.placeholder}
+             value={props.value}/>
+  )
+}
+
+export default Input
+
+
+//                 <div className="input-group bg-white p-1">
+//                     <textarea ref={this.inputRef}
+//                               className="form-control border-0 rounded-0"
+//                               onKeyPress={(e) => this.inputPress(e)}
+//                               placeholder="Type a message"/>
+//                     <div className="input-group-append">
+//                         <button
+//                             className="btn btn-outline-primary border-0 rounded-0"
+//                             onClick={(e) => this.btnClick(e)}>
+//                                 <FontAwesomeIcon icon={faPaperPlane}/>
+//                         </button>
+//                     </div>
+//                 </div>
